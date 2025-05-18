024b56a3ec441f53b5f53915c47dab60f3a11f86
GauravChanda7 <gauravchanda2022@gmail.com>
2025-05-18 23:10:02 +0000

    Create Login page and verify login data with DB

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'devsecretkey123'            #use random later to assign key
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notificationService.db'
 db = SQLAlchemy(app)
 
@@ -16,9 +17,9 @@ class UserBase(db.Model):
     password = db.Column(db.String(128), nullable = False)
     createdAt = db.Column(db.DateTime, default = lambda: datetime.now(timezone.utc))
     
-
     def __repr__(self):
         return f'<User {self.id}>'
+
 
 
 
@@ -28,35 +29,73 @@ def index():
         action = request.form.get('action')
 
         if action == 'Sign-in':
-            return render_template('signin.html')
+            return redirect('/signin')
         elif action == 'Login':
-            return render_template('login.html')
+            return redirect('/login')
         
     else:
         return render_template('index.html')
 
 
 
-@app.route('/signin', methods = ['POST'])
+@app.route('/signin', methods = ['GET', 'POST'])
 def signin():
-    name = request.form.get('name')
-    mailId = request.form.get('mailId')
-    phoneNumber = request.form.get('phoneNumber')
-    password = request.form.get('password')
+    if request.method == 'POST':
+        name = request.form.get('name')
+        mailId = request.form.get('mailId')
+        phoneNumber = request.form.get('phoneNumber')
+        password = request.form.get('password')
 
-    hashedPassword = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+        hashedPassword = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
 
-    newUser = UserBase(name = name, 
-                       mailId = mailId, 
-                       phoneNumber = phoneNumber, 
-                       password = hashedPassword)
+        newUser = UserBase(name = name, 
+                        mailId = mailId, 
+                        phoneNumber = phoneNumber, 
+                        password = hashedPassword)
+        
+        try:
+            db.session.add(newUser)
+            db.session.commit()
+            return redirect('/login')
+        except Exception as e:
+            return f'Error while sign-up: {e}'
+        
+    else:
+        return render_template('signin.html')
     
-    try:
-        db.session.add(newUser)
-        db.session.commit()
-        return redirect('/')
-    except Exception as e:
-        return f'Error while sign-up: {e}'
+
+    
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        mailId = request.form.get('mailId').strip()
+        password = request.form.get('password').strip()
+
+        user = UserBase.query.filter_by(mailId = mailId).first()
+        if user:
+            if check_password_hash(user.password, password):
+                return redirect('/homepage')
+            else:
+                flash('Incorrect password', 'error')
+                return redirect('/login')
+        else:
+            flash('Email not registered', 'error')
+            return redirect('/login')
+        
+    else:
+        return render_template('login.html')
+    
+
+
+@app.route('/homepage', methods = ['GET', 'POST'])
+def homepage():
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('homepage.html')
+
+
+
 
 if __name__ == '__main__':
     app.run(debug = True)
